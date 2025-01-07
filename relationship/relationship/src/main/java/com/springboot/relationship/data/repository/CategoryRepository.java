4ab688748852fb397df51827e19359a15b960f87
정민주 <145325581+mj12130@users.noreply.github.com>
2025-01-07 17:39:22 +0900

    Delete relationship/relationship directory

--- relationship/relationship/src/main/java/com/springboot/relationship/data/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.relationship.data.repository;
-
-import com.springboot.relationship.data.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-}
--- relationship/relationship/src/main/java/com/springboot/relationship/data/repository/ProducerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.relationship.data.repository;
-
-import com.springboot.relationship.data.entity.Producer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProducerRepository extends JpaRepository<Producer, Long> {
-
-}
